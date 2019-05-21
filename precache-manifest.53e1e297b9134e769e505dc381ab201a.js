f6c3a409f41a13ca8b96bcac05a4b6e32229bdc6
azaw502 <andrew502502@gmail.com>
2019-05-20 20:21:20 -0400

    Updates

---
+++ precache-manifest.53e1e297b9134e769e505dc381ab201a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4da2e8b3c4dd9792cb8aacd4ffbc18",
+    "url": "/rouxlette-demo/index.html"
+  },
+  {
+    "revision": "2d3a5bf921dca9e57d30",
+    "url": "/rouxlette-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rouxlette-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2d3a5bf921dca9e57d30",
+    "url": "/rouxlette-demo/static/js/main.e556df0a.chunk.js"
+  },
+  {
+    "revision": "1067dad226320224a30e",
+    "url": "/rouxlette-demo/static/js/runtime~main.13de2492.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rouxlette-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
